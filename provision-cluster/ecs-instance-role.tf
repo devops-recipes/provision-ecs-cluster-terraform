b5fc447c42ee2bd8b549810ad882631791668a44
ambarish2012 <ambarish@shippable.com>
2017-12-13 23:51:39 -0800

    update sample

--- provision-cluster/ecs-instance-role.tf
+++ provision-cluster/ecs-instance-role.tf
@@ -19,3 +19,12 @@ resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
     role       = "${aws_iam_role.ecs-instance-role.name}"
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
+
+resource "aws_iam_instance_profile" "ecs-instance-profile" {
+    name = "ecs-instance-profile"
+    path = "/"
+    role = "${aws_iam_role.ecs-instance-role.name}"
+    provisioner "local-exec" {
+      command = "sleep 10"
+    }
+}
--- provision-cluster/launch-configuration.tf
+++ provision-cluster/launch-configuration.tf
@@ -2,7 +2,18 @@ resource "aws_launch_configuration" "ecs-launch-configuration" {
     name                        = "ecs-launch-configuration"
     image_id                    = "ami-fad25980"
     instance_type               = "t2.xlarge"
-    iam_instance_profile        = "${aws_iam_role.ecs-instance-role.name}"
+    iam_instance_profile        = "${aws_iam_instance_profile.ecs-instance-profile.id}"
+
+    root_block_device {
+      volume_type = "standard"
+      volume_size = 100
+      delete_on_termination = true
+    }
+
+    lifecycle {
+      create_before_destroy = true
+    }
+
     security_groups             = ["${aws_security_group.test_public_sg.id}"]
     associate_public_ip_address = "true"
     key_name                    = "${var.ecs_key_pair_name}"
