7fcec717686e3cbfb2f7f6bc84a17da24aa2d2f6
ambarish2012 <ambarish@shippable.com>
2017-12-13 22:24:35 -0800

    update sample

--- provision-cluster/autoscaling-group.tf
+++ provision-cluster/autoscaling-group.tf
@@ -1,9 +1,9 @@
 
 resource "aws_autoscaling_group" "ecs-autoscaling-group" {
     name                        = "ecs-autoscaling-group"
-    max_size                    = "${var.max_instance-size}"
-    min_size                    = "${var.min_instance-size}"
-    desired_capacity            = "${var.desired-capacity}"
+    max_size                    = "${var.max_instance_size}"
+    min_size                    = "${var.min_instance_size}"
+    desired_capacity            = "${var.desired_capacity}"
     vpc_zone_identifier         = ["${aws_subnet.test_public_sn_01.id}", "${aws_subnet.test_public_sn_02.id}"]
     launch_configuration        = "${aws_launch_configuration.ecs-launch-configuration.name}"
     health_check_type           = "ELB"
--- provision-cluster/terraform.tfvars
+++ provision-cluster/terraform.tfvars
@@ -1,5 +1,5 @@
 ecs_cluster="$ECS_CLUSTER"
-ecs_key_pair_name="ECS_KEY_PAIR_NAME"
+ecs_key_pair_name="$ECS_KEY_PAIR_NAME"
 aws_access_key_id = "$PECT_AWS_KEYS_INTEGRATION_ACCESSKEY"
 aws_secret_access_key = "$PECT_AWS_KEYS_INTEGRATION_SECRETKEY"
 region = "$REGION"
